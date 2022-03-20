92fdc46ffc8ac59f0b95ce0a7a70d83dd93a65a1
zhimoe <xiongwengui67@gmail.com>
2022-03-20 14:26:23 +0000

    Update from https://github.com/zhimoe/zhimoe/commit/626bf327ecb6f8ddbca7701c37ea95ee9fc28674

--- js/search.js
+++ js/search.js
@@ -1,51 +1,53 @@
 $(document).ready(function () {
     // Popup Window;
-    var isfetched = false;
+    let isfetched = false;
     // Search DB path;
-    var search_path = "sitemap.xml";
-    var path = "/" + search_path;
+    let search_path = "sitemap.xml";
+    let path = "/" + search_path;
     // monitor main search box;
     function proceedsearch() {
       $("body").append('<div class="popoverlay">').css('overflow', 'hidden');
       $('.popup').toggle();
     }
     // search function;
-    var searchFunc = function(path, search_id, content_id) {
+    let searchFunc = function(path, search_id, content_id) {
         'use strict';
         $.ajax({
             url: path,
             dataType: "xml",
             async: true,
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+                console.log(textStatus);
+            },
             success: function( xmlResponse ) {
                 // get the contents from search data
-                console.log("xxxx");
                 isfetched = true;
                 $('.popup').detach().appendTo('.header-inner');
-                var datas = $( "entry", xmlResponse ).map(function() {
+                let datas = $( "entry", xmlResponse ).map(function() {
                     return {
                         title: $( "title", this ).text(),
                         content: $("content",this).text(),
                         url: $( "url" , this).text()
                     };
                 }).get();
-                var $input = document.getElementById(search_id);
-                var $resultContent = document.getElementById(content_id);
+                let $input = document.getElementById(search_id);
+                let $resultContent = document.getElementById(content_id);
                 $input.addEventListener('input', function(){
-                    var matchcounts = 0;
-                    var str='<ul class=\"search-result-list\">';
-                    var keywords = this.value.trim().toLowerCase().split(/[\s\-]+/);
+                    let matchcounts = 0;
+                    let str='<ul class=\"search-result-list\">';
+                    let keywords = this.value.trim().toLowerCase().split(/[\s\-]+/);
                     $resultContent.innerHTML = "";
                     if (this.value.trim().length > 1) {
                     // perform local searching
                     datas.forEach(function(data) {
-                        var isMatch = true;
-                        var content_index = [];
-                        var data_title = data.title.trim().toLowerCase();
-                        var data_content = data.content.trim().replace(/<[^>]+>/g,"").toLowerCase();
-                        var data_url = data.url;
-                        var index_title = -1;
-                        var index_content = -1;
-                        var first_occur = -1;
+                        let isMatch = true;
+                        let content_index = [];
+                        let data_title = data.title.trim().toLowerCase();
+                        let data_content = data.content.trim().replace(/<[^>]+>/g,"").toLowerCase();
+                        let data_url = data.url;
+                        let index_title = -1;
+                        let index_content = -1;
+                        let first_occur = -1;
                         // only match artiles with not empty titles and contents
                         if(data_title != '' && data_content != '') {
                             keywords.forEach(function(keyword, i) {
@@ -67,11 +69,11 @@ $(document).ready(function () {
                         if (isMatch) {
                             matchcounts += 1;
                             str += "<li><a href='"+ data_url +"' class='search-result-title'>"+ data_title +"</a>";
-                            var content = data.content.trim().replace(/<[^>]+>/g,"");
+                            let content = data.content.trim().replace(/<[^>]+>/g,"");
                             if (first_occur >= 0) {
                                 // cut out 100 characters
-                                var start = first_occur - 20;
-                                var end = first_occur + 80;
+                                let start = first_occur - 20;
+                                let end = first_occur + 80;
                                 if(start < 0){
                                     start = 0;
                                 }
@@ -81,10 +83,10 @@ $(document).ready(function () {
                                 if(end > content.length){
                                     end = content.length;
                                 }
-                                var match_content = content.substring(start, end);
+                                let match_content = content.substring(start, end);
                                 // highlight all keywords
                                 keywords.forEach(function(keyword){
-                                    var regS = new RegExp(keyword, "gi");
+                                    let regS = new RegExp(keyword, "gi");
                                     match_content = match_content.replace(regS, "<b class=\"search-keyword\">"+keyword+"</b>");
                                 });
 
