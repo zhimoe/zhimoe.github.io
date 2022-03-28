5009dc5a915e2cb4505cb5de0968945011a644c8
zhimoe <xiongwengui67@gmail.com>
2022-03-28 05:13:54 +0000

    Update from https://github.com/zhimoe/zhimoe/commit/0f24592b8927593ed4c41d49a0076e7ed390ea8c

--- js/search.js
+++ js/search.js
@@ -1,53 +1,52 @@
 $(document).ready(function () {
     // Popup Window;
-    let isfetched = false;
+    var isfetched = false;
     // Search DB path;
-    let search_path = "sitemap.xml";
-    let path = "/" + search_path;
+    // TO-DO Seems not found best way 
+    var pre_sch_path = window.location.href.indexOf('/en/') > -1 ? '/en/' : "/"
+    var search_path = "searchindex.xml";
+    var path = pre_sch_path + search_path;
     // monitor main search box;
     function proceedsearch() {
-      $("body").append('<div class="popoverlay">').css('overflow', 'hidden');
-      $('.popup').toggle();
+      $("body").append('<div class="popoverlay local-search-pop-overlay">').css('overflow', 'hide');
+      $('.popup').fadeIn('slow');
     }
     // search function;
-    let searchFunc = function(path, search_id, content_id) {
+    var searchFunc = function(path, search_id, content_id) {
         'use strict';
         $.ajax({
             url: path,
-            dataType: "xml",
+            dataType: "text",
             async: true,
-            error: function(XMLHttpRequest, textStatus, errorThrown) {
-                console.log(textStatus);
-            },
             success: function( xmlResponse ) {
-                // get the contents from search data
+                // get the contents from search data                
                 isfetched = true;
                 $('.popup').detach().appendTo('.header-inner');
-                let datas = $( "entry", xmlResponse ).map(function() {
+                var datas = $( "entry", xmlResponse ).map(function() {
                     return {
                         title: $( "title", this ).text(),
                         content: $("content",this).text(),
                         url: $( "url" , this).text()
                     };
                 }).get();
-                let $input = document.getElementById(search_id);
-                let $resultContent = document.getElementById(content_id);
+                var $input = document.getElementById(search_id);
+                var $resultContent = document.getElementById(content_id);
                 $input.addEventListener('input', function(){
-                    let matchcounts = 0;
-                    let str='<ul class=\"search-result-list\">';
-                    let keywords = this.value.trim().toLowerCase().split(/[\s\-]+/);
+                    var matchcounts = 0;
+                    var str='<ul class=\"search-result-list\">';
+                    var keywords = this.value.trim().toLowerCase().split(/[\s\-]+/);
                     $resultContent.innerHTML = "";
                     if (this.value.trim().length > 1) {
                     // perform local searching
                     datas.forEach(function(data) {
-                        let isMatch = true;
-                        let content_index = [];
-                        let data_title = data.title.trim().toLowerCase();
-                        let data_content = data.content.trim().replace(/<[^>]+>/g,"").toLowerCase();
-                        let data_url = data.url;
-                        let index_title = -1;
-                        let index_content = -1;
-                        let first_occur = -1;
+                        var isMatch = true;
+                        var content_index = [];
+                        var data_title = data.title.trim().toLowerCase();
+                        var data_content = data.content.trim().replace(/<[^>]+>/g,"").toLowerCase();
+                        var data_url = data.url;
+                        var index_title = -1;
+                        var index_content = -1;
+                        var first_occur = -1;
                         // only match artiles with not empty titles and contents
                         if(data_title != '' && data_content != '') {
                             keywords.forEach(function(keyword, i) {
@@ -68,12 +67,16 @@ $(document).ready(function () {
                         // show search results
                         if (isMatch) {
                             matchcounts += 1;
+                            keywords.forEach(function(keyword){
+                                var regS = new RegExp(keyword, "gi");
+                                data_title = data_title.replace(regS, "<span class=\"search-title-keyword\">" + keyword + "</span>");
+                            });
                             str += "<li><a href='"+ data_url +"' class='search-result-title'>"+ data_title +"</a>";
-                            let content = data.content.trim().replace(/<[^>]+>/g,"");
+                            var content = data.content.trim().replace(/<[^>]+>/g,"");
                             if (first_occur >= 0) {
                                 // cut out 100 characters
-                                let start = first_occur - 20;
-                                let end = first_occur + 80;
+                                var start = first_occur - 20;
+                                var end = first_occur + 80;
                                 if(start < 0){
                                     start = 0;
                                 }
@@ -83,10 +86,10 @@ $(document).ready(function () {
                                 if(end > content.length){
                                     end = content.length;
                                 }
-                                let match_content = content.substring(start, end);
+                                var match_content = content.substring(start, end);
                                 // highlight all keywords
                                 keywords.forEach(function(keyword){
-                                    let regS = new RegExp(keyword, "gi");
+                                    var regS = new RegExp(keyword, "gi");
                                     match_content = match_content.replace(regS, "<b class=\"search-keyword\">"+keyword+"</b>");
                                 });
 
@@ -96,8 +99,20 @@ $(document).ready(function () {
                         }
                     })};
                     str += "</ul>";
-                    if (matchcounts == 0) { str = '<div id="no-result"><i class="fa fa-frown-o fa-5x" /></div>' }
-                    if (keywords == "") { str = '<div id="no-result"><i class="fa fa-search fa-5x" /></div>' }
+
+                    var rs_cnt = "<div class='search-stats'><b>"+matchcounts
+                    if (pre_sch_path == '/') {
+                        rs_cnt += " </b>个结果被找到！"
+                    } else {
+                        rs_cnt += " </b>results found!"
+
+                    }
+                    rs_cnt += "</div><hr/>"
+                    str = rs_cnt + str
+
+                    if (matchcounts == 0) { str = '<div id="no-result"><i class="fa fa-frown-o fa-5x" /></div>' }                    
+                    if (keywords == "") { str = '<div id="no-result"><i class="fa fa-search fa-5x" /></div>' } 
+
                     $resultContent.innerHTML = str;
                 });
                 proceedsearch();
@@ -107,17 +122,19 @@ $(document).ready(function () {
 
     // handle and trigger popup window;
     $('.popup-trigger').click(function(e) {
-     console.log("isfetched", isfetched);
-      e.stopPropagation();
-      if (isfetched == false) {
-        searchFunc(path, 'local-search-input', 'local-search-result');
-      } else {
-        proceedsearch();
-      };
+        e.stopPropagation();
+        //TODO why here need timeout, couldn't understand it.
+        setTimeout(() => $('#local-search-input').focus(), 500);
+      
+        if (isfetched == false) {
+            searchFunc(path, 'local-search-input', 'local-search-result');
+        } else {     
+            proceedsearch();
+        };
     });
 
     $('.popup-btn-close').click(function(e){
-      $('.popup').hide();
+      $('.popup').fadeOut('slow');
       $(".popoverlay").remove();
       $('body').css('overflow', '');
     });
